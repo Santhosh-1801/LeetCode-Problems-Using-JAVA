49a40421bef636fa1f81ee10d0ec3304f5444660
Santhosh Kumar <74703503+Santhosh-1801@users.noreply.github.com>
2022-08-20 23:38:01 +0530

    Time: 13 ms (6.95%), Space: 42.6 MB (39.79%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.println(nums[i]);
+        }
+        
+    }
+}
