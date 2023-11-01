1baa2248b61cb74a473ff7bba56a25c941661035
Santhosh Kumar <74703503+Santhosh-1801@users.noreply.github.com>
2023-11-01 19:35:14 +0530

    Update InputMismatchException.java

--- Exceptions/InputMismatchException.java
+++ Exceptions/InputMismatchException.java
@@ -1,9 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
 import java.util.*;
 
 public class Main
 {
 	public static void main(String[] args) {
-	 System.out.print(getTheValue());
+	 System.out.println(getTheValue());
+	 System.out.println(getTheCorrect());
 	}
 	static int getTheValue()
 	{
@@ -14,6 +22,25 @@ public class Main
 	    }
 	    catch(Exception e){
 	        System.out.println(e);
+	        return 0;
+	    }
+	}
+	static int getTheCorrect(){
+	    Scanner sc=new Scanner(System.in);
+	    boolean valid=true;
+	    System.out.println("Enter an value");
+	    String valueget=sc.next();
+	    for(int i=0;i<valueget.length();i++)
+	    {
+	        if(!Character.isDigit(valueget.charAt(i))){
+	            valid=false;
+	            break;
+	        }
+	    }
+	    if(valid){
+	        return Integer.parseInt(valueget);
+	    }
+	    else{
 	        return 0;
 	    }
 	}
