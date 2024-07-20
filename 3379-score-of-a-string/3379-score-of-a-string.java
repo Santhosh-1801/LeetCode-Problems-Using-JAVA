e3efdfddfc0ff4e6e379421e06793313b25b460c
Santhosh Kumar <74703503+Santhosh-1801@users.noreply.github.com>
2024-07-20 21:26:56 +0530

    Time: 1 ms (99.48%), Space: 42.1 MB (20.63%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int scoreOfString(String s) {
+
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            sum=sum+(Math.abs((s.charAt(i)-'a')-(s.charAt(i+1)-'a')));
+        }
+        return sum;
+        
+    }
+}
